e3dd8063a5feeef331a7068d976cd8dbf90faea4
ErikBouvier <pablobb.prog@gmail.com>
2024-11-23 12:01:35 -0300

    cambio de nombre

---
+++ pre-entraga2/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import routerProducts from './routes/product.router.js';
+import routerCarts from './routes/cart.router.js';
+import routerViewHome from './routes/home.view.router.js';
+import { config as configHandlebars } from './config/handlebars.config.js';
+import { config as configWesocket } from './config/websocket.config.js';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+configHandlebars(app);
+
+app.use('/api/public', express.static('./src/public'));
+app.use('/', routerViewHome);
+app.use('/api/products', routerProducts);
+app.use('/api/carts', routerCarts);
+
+app.use('*', (req, res) => {
+    res.status(404).render('error404', { error: 'Not found' });
+})
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+configWesocket(httpServer); 
